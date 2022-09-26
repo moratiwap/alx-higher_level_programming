a3bdd72b16cdf2f8c2fcfdc0bb9f3cfaddcd3209
moratiwap <moratiwapearl@gmail.com>
2022-09-26 09:32:03 -0700

    Inheritance in Python the snake

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,27 +1,38 @@
 #!/usr/bin/python3
 """
-Contains BaseGeometry
-with public instance method area and integer_validation
+This module implements a base class for Geometry objects,
+with attribute validator functions
+
+Raises:
+    Exception: area is not implemented
+    TypeError: [attribute] must be an integer
+    ValueError: [attribute] ,must be greater than 0
 """
 
 
 class BaseGeometry:
-    """
-    Methods:
-        area(self)
-        integer_validator(self, name, value)
+    """base class
     """
     def area(self):
-        """not implemented"""
+        """find area
+
+        Raises:
+            Exception: area not implemented
+        """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """validates input
+        """check value
+
         Args:
-            name (str): assumed always a string
-            value (int): greater than 0
+            name (str): value designation
+            value (any): value to check
+
+        Raises:
+            TypeError: value must be an integer
+            ValueError: value must be greater than 0
         """
-        if not isinstance(value, int):
-            raise TypeError("{:s} must be an integer".format(name))
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("{:s} must be greater than 0".format(name))
+            raise ValueError("{} must be greater than 0".format(name))
